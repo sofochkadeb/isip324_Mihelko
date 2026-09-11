e31274de08cf0bbc6e3a3d6c2f8f18de6c55fcbc
sofochkadeb <sofia_krum@mail.ru>
2026-09-11 13:42:43 +0300

    Сделала два списка отдельных для названия траты и суммы. Потом цикл do while на перебор знач от 2 до 40. Потом ввод строки и разделение на 2 части.

--- isip324_Mihelko/Program.cs
+++ isip324_Mihelko/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization.Formatters;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,19 @@ namespace isip324_Mihelko
     {
         static void Main(string[] args)
         {
+            var name = new List<string>();
+            var price = new List<double>();
+            int n;
+            do
+            {
+                Console.WriteLine("Введите количество операций (2-40): ");
+            } while (!int.TryParse(Console.ReadLine(), out n) || n < 2 || n > 40);
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Трата #{i + 1} (Название; Сумма): ");
+                string line = Console.ReadLine();
+                string[] parts = line.Split(';');
+            }
         }
     }
 }
